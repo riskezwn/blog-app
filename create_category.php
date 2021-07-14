382420eae0a8f49bb7c22671e8aa60341119cb89
Riskezwn <enriquegarciagasc@gmail.com>
2021-07-14 08:45:29 +0200

    format

--- create_category.php
+++ create_category.php
@@ -8,22 +8,17 @@ require_once('includes/header.php');
     <h3>Crear categoría</h3>
     <hr class="main-hr" />
 
-    <form class="create-category "action="savecategory.php" method="post">
-        <?php if (isset($_SESSION['category_error'])) {
-          echo checkCreateCategoryError($_SESSION['category_error']);
+    <form class="create-category " action="savecategory.php" method="post">
+      <?php if (isset($_SESSION['category_error'])) {
+        echo checkCreateCategoryError($_SESSION['category_error']);
+      }
+      ?>
+      <label for="category_name">Introduce el nombre la categoría que quieres crear</label>
+      <input type="text" name="category_name" id="category_name">
 
-        }
-        
-        
-        ?>
-        
-        <label for="category_name">Introduce el nombre la categoría que quieres crear</label>
-        <input type="text" name="category_name" id="category_name">
-        
-        <input class="btn" type="submit" value="Crear">
+      <input class="btn" type="submit" value="Crear">
     </form>
     <?php deleteSession('category_error') ?>
-   
   </section>
   <?php
   require_once('includes/aside.php')
