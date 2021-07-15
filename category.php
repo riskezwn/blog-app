19ed7ae530c9ae26cec259f73a7ba57ea8de4d2b
Riskezwn <enriquegarciagasc@gmail.com>
2021-07-15 08:46:48 +0200

    fix empty categories

--- category.php
+++ category.php
@@ -17,55 +17,55 @@ if (isset($_GET['id']) && sanitizeNum($con, $_GET['id'])) {
 
 <main>
   <section class="news">
-    <h3><?=$category_name?></h3>
+    <h3><?= $category_name ?></h3>
     <hr class="main-hr" />
-    <?php 
-    $entries = getEntries($con, null, $category_id);
-    if (mysqli_num_rows($entries) > 0) : ?>
-    <div class="last-entries" id="lastEntries">
-      <?php
-      if ($entries = getEntries($con, 4, $category_id)) :
-        while ($entry = mysqli_fetch_assoc($entries)) :
-      ?>
-          <a href="entry.php?id=<?= $entry['id'] ?>">
-            <article>
-              <h4 class="news-title"><?= $entry['title'] ?></h4>
-              <p class="news-category"><?= $entry['category_name'] ?> <span class="news-date"><?= $entry['entry_date'] ?></span></p>
-              <p class="news-body">
-                <?= substr($entry['description'], 0, 350) . '...' ?>
-              </p>
-            </article>
-          </a>
-      <?php
-        endwhile;
-      endif;
-      ?>
-    </div>
-    <div class="all-entries" id="allEntries" style="display: none;">
-      <?php
-      if ($entries = getEntries($con, null , $category_id)) :
-        while ($entry = mysqli_fetch_assoc($entries)) :
-      ?>
-          <a href="entry.php?id=<?= $entry['id'] ?>">
-            <article>
-              <h4 class="news-title"><?= $entry['title'] ?></h4>
-              <p class="news-category"><?= $entry['category_name'] ?> <span class="news-date"><?= $entry['entry_date'] ?></span></p>
-              <p class="news-body">
-              <?= substr($entry['description'], 0, 350) . '...' ?>
-              </p>
-            </article>
-          </a>
-      <?php
-        endwhile;
-      endif;
-      ?>
-    </div>
-    <div>
-      <a href="#" class="btn" id="getAllEntries">Ver todas las noticias <i class="fas fa-arrow-right"></i></a>
-    </div>
+    <?php
+
+    if (getEntries($con, null, $category_id)) : ?>
+      <div class="last-entries" id="lastEntries">
+        <?php
+        if ($entries = getEntries($con, 4, $category_id)) :
+          while ($entry = mysqli_fetch_assoc($entries)) :
+        ?>
+            <a href="entry.php?id=<?= $entry['id'] ?>">
+              <article>
+                <h4 class="news-title"><?= $entry['title'] ?></h4>
+                <p class="news-category"><?= $entry['category_name'] ?> <span class="news-date"><?= $entry['entry_date'] ?></span></p>
+                <p class="news-body">
+                  <?= substr($entry['description'], 0, 350) . '...' ?>
+                </p>
+              </article>
+            </a>
+        <?php
+          endwhile;
+        endif;
+        ?>
+      </div>
+      <div class="all-entries" id="allEntries" style="display: none;">
+        <?php
+        if ($entries = getEntries($con, null, $category_id)) :
+          while ($entry = mysqli_fetch_assoc($entries)) :
+        ?>
+            <a href="entry.php?id=<?= $entry['id'] ?>">
+              <article>
+                <h4 class="news-title"><?= $entry['title'] ?></h4>
+                <p class="news-category"><?= $entry['category_name'] ?> <span class="news-date"><?= $entry['entry_date'] ?></span></p>
+                <p class="news-body">
+                  <?= substr($entry['description'], 0, 350) . '...' ?>
+                </p>
+              </article>
+            </a>
+        <?php
+          endwhile;
+        endif;
+        ?>
+      </div>
+      <div>
+        <a href="#" class="btn" id="getAllEntries">Ver todas las noticias <i class="fas fa-arrow-right"></i></a>
+      </div>
     <?php else : ?>
       <p>Nada que ver por aquí...</p>
-    
+
     <?php endif; ?>
   </section>
   <?php
